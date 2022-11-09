e4b581b577dfdee3c58122b6c341f36cfd62e70a
Will Freeman <hohosanta@me.com>
2022-11-08 20:33:26 -0600

    Initial Commit

---
+++ RailRider/RailRiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RailRiderApp.swift
+//  RailRider
+//
+//  Created by Will Freeman on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct RailRiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
