99e4b78ba8de0b0e2a2db331404174316b2102ca
Will Freeman <will.freeman@hillmangroup.com>
2022-12-17 15:27:48 -0600

    display crossings

--- RailRider/ContentView.swift
+++ RailRider/ContentView.swift
@@ -8,15 +8,54 @@
 import SwiftUI
 import MapKit
 
-struct OSMRailway: Hashable, Codable {
+struct OSMRailway: Hashable, Decodable {
     let name: String
     let id: Int
 }
 
+struct GeoJsonAPIResponse: Hashable, Decodable {
+    let geometry: GeoJsonGeometry
+}
+
+struct GeoJsonGeometry: Hashable, Decodable {
+    let coordinates: [[Double]]
+}
+
+struct Crossing: Identifiable {
+    let id = UUID()
+    var coordinates: CLLocationCoordinate2D
+}
+
 class RailwayModel: ObservableObject {
     @Published var nearbyRailways: [OSMRailway] = []
+    @Published var crossings: [Crossing] = []
     
-    public func fetchRailways() {
+    func fetchCrossings(railwayId: Int) {
+        guard let url = URL(string: "http://localhost:3000/railroad/\(railwayId)/crossings") else {
+                return
+            }
+        print(url.absoluteString)
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            // convert to JSON
+            do {
+                let apiResponse = try JSONDecoder().decode(GeoJsonAPIResponse.self, from: data)
+                self.crossings = apiResponse.geometry.coordinates.map {
+                    Crossing(coordinates: CLLocationCoordinate2D(latitude: $0[1], longitude: $0[0]))
+                }
+            } catch {
+                print(error)
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func fetchRailways() {
         guard let url = URL(string: "http://localhost:3000/nearestRailroad?coordinates=34.627103,-86.970355&radius=100") else {
             return
         }
@@ -47,7 +86,12 @@ struct ContentView: View {
     
     var body: some View {
         ZStack(alignment: .top) {
-            Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+            Map(coordinateRegion: $viewModel.region, showsUserLocation: true, userTrackingMode: .constant(.follow), annotationItems: railwayModel.crossings) {
+                crossing in
+                MapAnnotation(coordinate: crossing.coordinates) {
+                    Label("asshat", systemImage: "tram")
+                }
+            }
                 .ignoresSafeArea()
                 .onAppear {
                     viewModel.initLocationManager()
@@ -58,8 +102,15 @@ struct ContentView: View {
                         ForEach(railwayModel.nearbyRailways, id: \.self) { railway in
                             Label(railway.name, systemImage: "tram").tag(railway as OSMRailway?)
                         }
-                    }.padding(10).background(.white).clipShape(RoundedRectangle(cornerRadius: 10))
-                        .shadow(radius: 3)
+                    }
+                    .padding(10).background(.white).clipShape(RoundedRectangle(cornerRadius: 10))
+                    .shadow(radius: 3)
+                    .onChange(of: currentRailway, perform: { newRailway in
+                        guard let railway = newRailway else {
+                            return
+                        }
+                        railwayModel.fetchCrossings(railwayId: railway.id)
+                    })
                 } else {
                     Button("Find Nearby Railway", action: railwayModel.fetchRailways)
                         .buttonStyle(.borderedProminent)
@@ -83,7 +134,7 @@ struct ContentView_Previews: PreviewProvider {
 
 final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
     var locationManager: CLLocationManager?
-    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 34, longitude: -86), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 34, longitude: -86), span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
     
     func initLocationManager() {
         if (CLLocationManager.locationServicesEnabled()) {
